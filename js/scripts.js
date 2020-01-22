400425adc62490278a7cce837d395f3852c7825a
Antonio Cruz and Isaac Garnand <antonio@>
2020-01-22 10:58:32 -0800

    added html page with form, changed business logic to make it more DRY

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-var a = parseInt(prompt("Enter a number: "));
-var b = parseInt(prompt("Enter another number: "));
-var c = a % b
-
-for (var index = 0; ((index * b) + c) < a; index += 1);
-
-  
-  if (c === 0) {
-  alert('the multiple of ' + a + 'times ' + b + 'is ' + index + " with no remainder");
-  }
-    else {
-    alert('the multiple of ' + a + 'times ' + b + 'is ' + index + "with a remainder of" + c);
+$(document).ready(function() {
+  $("form#loop").submit(function(event){
+    event.preventDefault();
+    var a = parseInt($("input#number").val());
+    var b = parseInt($("input#multiple").val());
+    var c = a % b
+    var array = [];
+    
+    for (var i = 1; ((i * b) + c) <= a; i += 1) {
+    var d = i * b;
+    array.push(d);
     }
+          
+    $(".output").append('the multiple of ' + a + ' times ' + b + ' is ' + array.length + " with a remainder of " + c + ". <br> Your multiples are " + array + ".");
+   
+    
+  });
+});
+
